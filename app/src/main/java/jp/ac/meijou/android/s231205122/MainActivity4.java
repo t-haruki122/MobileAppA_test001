ebe03bc6745b51cf499d936163634dfb70a2ffaa
t-haruki122 m <231205122@ccmailg.meijo-u.ac.jp>
2024-09-04 16:09:30 +0900

    20240904-morning create calc

--- app/src/main/java/jp/ac/meijou/android/s231205122/MainActivity4.java
+++ app/src/main/java/jp/ac/meijou/android/s231205122/MainActivity4.java
@@ -8,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import jp.ac.meijou.android.s231205122.databinding.ActivityMain2Binding;
-
-public class MainActivity2 extends AppCompatActivity {
-    private ActivityMain2Binding binding;
-
-    private int showData = 0;
-    private int memoryData = 0;
-    private int operand = 0; // PLU: 0, MIN: 1, x: 2, DIV: 3
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main4);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-
     }
 }
